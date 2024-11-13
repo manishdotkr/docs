26dd302e59b680eb61e935df2eb653bbb7aca8d8
manishdotkr <officialmanishkr98@gmail.com>
2024-11-13 12:29:58 +0530

    update: updated next.config.js to create static build files

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '',
+  trailingSlash: true,
+})
