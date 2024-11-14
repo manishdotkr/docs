4090e7f1b77bfdd1f3fe5cd58c9e312681c780d0
manishdotkr <officialmanishkr98@gmail.com>
2024-11-14 18:06:47 +0530

    add: added all the documents

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Docs</span>,
+  // logo: <span>Docs</span>,
+  logo: <img src="/assets/logo.png" alt="Logo" style={{ width: '120px', height: 'auto' }} />,
   project: {
     link: 'https://github.com/manishdotkr',
-  },
-  chat: {
-    link: 'https://www.linkedin.com/in/manishdotkr',
   },
   docsRepositoryBase: 'https://github.com/manishdotkr/docs',
   footer: {
     text: 'Nextra Docs Template',
   },
+  sidebar: {
+    defaultMenuCollapseLevel: 1, // Set to 1 to collapse all by default
+    titleComponent: ({ title, type }) => <>{title}</>,
+  },
 }
 
-export default config
+export default config;
